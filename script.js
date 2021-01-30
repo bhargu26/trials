63cc1c9dd0152572f8335b3f3eb25c68701d7a9f
bhargu26 <76743318+bhargu26@users.noreply.github.com>
2021-01-30 20:24:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["ECE-Thirdie","Coder","Designer"],
+        strings: ["Manitian","Coder","Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
